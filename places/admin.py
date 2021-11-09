cfc384cae18608f5e25c23022afdbd9b59cfeada
J0hn <j0hntvdesign@gmail.com>
2021-11-09 16:58:26 +0300

    Add place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Place
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    readonly_fields = ['updated_at']
--- places/models.py
+++ places/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    address = models.CharField('Адрес', max_length=128, unique=True)
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+
+    def __str__(self):
+        return self.address
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
