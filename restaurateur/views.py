01eb3e2924a19d74d697fad003ff7df0eab6b603
J0hn <j0hntvdesign@gmail.com>
2021-11-10 12:15:36 +0300

    Remove similar queries from manager product page

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -66,7 +66,7 @@ def is_manager(user):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_products(request):
     restaurants = list(Restaurant.objects.order_by('name'))
-    products = list(Product.objects.prefetch_related('menu_items'))
+    products = list(Product.objects.prefetch_related('menu_items', 'category'))
 
     default_availability = {restaurant.id: False for restaurant in restaurants}
     products_with_restaurants = []
