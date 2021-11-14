789023151dac3e11a0f5c9112ed3583a6390d2eb
J0hn <j0hntvdesign@gmail.com>
2021-11-14 22:21:11 +0300

    Fix place coordinates fields to optional

--- places/models.py
+++ places/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Place(models.Model):
     address = models.CharField('Адрес', max_length=128, unique=True, db_index=True)
-    lat = models.FloatField('Широта')
-    lon = models.FloatField('Долгота')
+    lat = models.FloatField('Широта', null=True, blank=True)
+    lon = models.FloatField('Долгота', null=True, blank=True)
     updated_at = models.DateTimeField('Дата обновления', auto_now=True)
 
     class Meta:
