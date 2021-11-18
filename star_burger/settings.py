b69efb7aa1c3e6fd544f0749a277a4add6f1bb13
J0hn <j0hntvdesign@gmail.com>
2021-11-18 20:27:53 +0300

    Rename static root

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -9,7 +9,7 @@ env = Env()
 env.read_env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
